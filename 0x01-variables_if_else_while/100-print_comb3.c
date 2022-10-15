4e4a8dd415fb90aad62fff037500d10f5ea2964e
safaa1995 <eng.safaamustafa@gmail.com>
2022-10-15 10:52:14 -0700

    10 2 chance

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 
 	c = i = '0';
 
-	for (c = '0'; c < '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
 			putchar(c);
 		       putchar(i);
 
-			if ((c != '9') || (c == '9' && i != '9'))
+			if ((c != '9') || (c == '8' && i != '9'))
 			{
 				putchar(',');
 			       putchar(' ');
