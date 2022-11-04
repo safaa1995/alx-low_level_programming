42a97dfbf73873fc9689c7c96e0f7338dd9e3223
safaa1995 <eng.safaamustafa@gmail.com>
2022-11-04 10:50:23 -0700

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,14 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int count = 0;
+	int i;
 
-	if (argc > 0)
-	{
-		while (count < argc)
-		{
-			printf("%s\n", argv[count]);
-		}
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
 	return (0);
 }
