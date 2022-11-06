feb25271316646528034bbe77f8810b8bfc68934
safaa1995 <eng.safaamustafa@gmail.com>
2022-11-06 11:42:36 -0800

    task0000

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,10 +1,11 @@
 #include "main.h"
-
+#include <stdio.h>
+/**
+ * main - Print the name of the program
+ * return  0  (Success)
+ */
 int main(void)
 {
-
-	    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-
-	        return (0);
-
+	_puts("\"At the end of the day, my goal wa");
+	return (0);
 }
