b290c55f0c7a4a1208e5ed54df5b97068b31799f
safaa1995 <eng.safaamustafa@gmail.com>
2022-11-08 07:47:50 -0800

    task44

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,31 +11,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, length, sum;
-	char *ptr;
+	int i, j, z;
+	int sum = 0;
 
-	if (argc < 2)
-		printf("0\n");
-	else
+	for (j = 1; j < argc; j++)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
+		for (z = 0; argv[j][z] != '\0'; z++)
 		{
-			ptr = argv[i];
-			length = strlen(ptr);
-
-			for (j = 0; j < length; j++)
+			if (argv[j][z] < '0' || argv[j][z] > '9')
 			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			sum += atoi(argv[i]);
 		}
-
-		printf("%d\n", sum);
 	}
+	for (i = 1; i < argc; i++)
+		sum += atoi(argv[i]);
+	printf("%d\n", sum);
 	return (0);
 }
