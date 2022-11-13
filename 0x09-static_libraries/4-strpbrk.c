92dd104a75b9b4c8df0957cc102116995f8f3e35
safaa1995 <eng.safaamustafa@gmail.com>
2022-11-13 08:39:26 -0800

    p

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,11 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _isalpha - checks for lowercase character
  * @c: parameter is hard-coded in main function
  * Return: 0 or 1
  */
-char *_strpbrk(char *s, char *accept)
+char *_strpbrk(char *is, char *accept)
 {
+	int size = 0;
+	char *ptr = s;
+	char *tmp = accept;
+	int i;
 
+	while (*accept++)
+		size++;
+	accept = tmp;
+
+	while (*s)
+	{
+		accept = tmp;
+		i = 0 ;
+		while (accept < tmp + size)
+		{
+			if (*s == *accept)
+				ptr = s, i++;
+			accept++;
+
+		}
+		if (i == 1)
+			return (ptr);
+		s++;
+	}
+	return (NULL);
 }
